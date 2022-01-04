bb267d5db2de89f16fc68125c3ef610e5deafaae
jeffxhansen <jeffxhansen@gmail.com>
2022-01-04 12:29:41 -0700

    basic layout

--- js/init.js
+++ js/init.js
@@ -1,10 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('#demo-carousel');
-    var options = {fullWidth: true, indicators: true, noWrap: true, padding: -100, duration: 100}
-    var instances = M.Carousel.init(elems, options);
-});
-document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('#card-carousel');
-    var options = { fullWidth: true, indicators: true, noWrap: true }
+    var options = { fullWidth: true, indicators: true, noWrap: true, padding: -80, duration: 50 }
     var instances = M.Carousel.init(elems, options);
 });
